29d52197eb22580ed090dda19c8e222cfc83188a
Matt Windle <matthewwindle@ekm.com>
2021-07-04 17:41:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import background from './image.jpg';
+import portal from './rickportal.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <aside style={{ backgroundImage: 'url(' + background + ')' }}>
+          <div className="aside-content">
+            <h1>Search for characters, episodes and locations</h1>
+            <form action="">
+              <input type="text" placeholder="Rick Sanchez" />
+            </form>
+          </div>
+          <img className="portal" src={portal} alt="" />
+        </aside>
+        <main>
+          <h2>Characters</h2>
+        </main>
+      </div>
     </div>
   );
 }
 
 export default App;
+
